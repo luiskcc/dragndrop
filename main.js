85b9dfa287475ee2db0528237f7b7aa09d4f3e61
“luiskcc” <114909465+luiskcc@users.noreply.github.com>
2023-05-21 17:39:28 +0100

    list event listeners

--- main.js
+++ main.js
@@ -5,19 +5,36 @@ let draggedItem = null;
 
 for (let i = 0; i < list_items.length; i++) {
     const item = list_items[i]
-};
 
-item.addEventListener('dragstart', function (){
-    let draggedItem;
-    console.log('dragstart');
-    draggedItem = this;
-})
 
-item.addEventListener('dragend', function(){
-    console.log('dragend');
-    draggedItem = null; 
-}) 
+    item.addEventListener('dragstart', function (){
+        draggedItem = item;
+        setTimeout(function () {
+            item.style.display = "none";
+        }, 0)
+    });
 
-for (let j = 0; j < lists.length; i++){
-    const  list = lists[j]
-};
+    item.addEventListener('dragend', function(){
+        setTimeout(function() {
+            draggedItem.style.display = 'block';
+            draggedItem = null;
+        }, 0);
+    }); 
+
+    for (let j = 0; j < lists.length;  j ++){
+        const  list = lists[j];
+
+        list.addEventListener('dragover', function (e) {
+			e.preventDefault();
+		});
+
+        list.addEventListener('dragenter', function (e) {
+            e.preventDefault();
+        })
+
+        list.addEventListener('drop', function (e) {
+            this.append(draggedItem);
+        });
+
+    }
+}
